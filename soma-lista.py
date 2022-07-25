1eff77f6b0615e3cfe79221db39b5c9346097099
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 14:12:17 -0300

    Create soma-lista.py

---
+++ soma-lista.py
@@ -0,0 +1,8 @@
+# Escreva a função soma_elementos que recebe como parâmetro uma lista com números inteiros e devolve um número inteiro correspondente à soma dos elementos da lista recebida.
+def soma_elementos(list):
+    result=int()
+    for i in list:
+        result+=i
+    return result
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
